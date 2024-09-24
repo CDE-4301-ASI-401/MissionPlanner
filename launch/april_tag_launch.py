a6d713300130d15345acecde80e893d417883de5
David <e1129809@u.nus.edu>
2024-09-24 11:59:16 +0800

    Refactor code and add type hints

--- launch/april_tag_launch.py
+++ launch/april_tag_launch.py
@@ -15,44 +15,61 @@ drone_ids = ["cf01","cf02","cf03","cf04","cf05","cf06","cf07","cf08","cf09","cf1
 #drone_ids = ["cf04"]
 #drone_ids = ["cf01"]
 
-#test
 
+def generate_launch_description() -> LaunchDescription:
+    nodes: list[Node] = []
 
-def generate_launch_description():
-    nodes = []
-    for i in drone_ids:
-        namespace = i
-        ai_deck_wrapper = Node(
-                package='ai_deck_wrapper',
-                executable='ai_deck_wrapper',
-                namespace=namespace,
-                output='screen',
-                parameters=[{
-                    'period': 0.1,
-                    'ip': f'192.168.1.1{namespace[2:]}',
-                    'name': f'{namespace}'  
-                }],
-                remappings=[
-                    ('/image_rect', f'/{namespace}/image_rect'),
-                    ('/camera_info', f'/{namespace}/camera_info'),
-                ])
-        nodes.append(ai_deck_wrapper)
-        apriltag_node = Node(
-                package='apriltag_ros',
-                executable='apriltag_node',
-                namespace=f'{namespace}',
-                output='screen',
-                remappings=[
-                    ('/image_rect', f'/{namespace}/image_rect'),
-                    ('/camera_info', f'/{namespace}/camera_info'),
-                    ('/tf',f'/{namespace}/tf'),
-                    ('/detections',f'/{namespace}/detections'),
-                ]
-                )
+    for drone_id in drone_ids:
+        ai_deck_wrapper_node : Node = get_ai_deck_wrapper_node(drone_id)
+        nodes.append(ai_deck_wrapper_node)
+
+        apriltag_node : Node = get_apriltag_node(drone_id)
         nodes.append(apriltag_node)
 
     # MISSION PLANNER
-    mission_planner = Node(
+    mission_planner_node : Node = 
+
+    # Create the launch description and populate
+    launch_description : LaunchDescription = LaunchDescription()
+
+    for node in nodes:
+        launch_description.add_action(node)
+
+    launch_description.add_action(mission_planner_node)
+    return launch_description
+
+def get_ai_deck_wrapper_node(drone_id : str) -> Node:
+    return Node(
+                package='ai_deck_wrapper',
+                executable='ai_deck_wrapper',
+                namespace=drone_id,
+                output='screen',
+                parameters=[{
+                    'period': 0.1,
+                    'ip': f'192.168.1.1{drone_id[2:]}',
+                    'name': f'{drone_id}'  
+                }],
+                remappings=[
+                    ('/image_rect', f'/{drone_id}/image_rect'),
+                    ('/camera_info', f'/{drone_id}/camera_info'),
+                ])
+
+def get_apriltag_node(drone_id: str) -> Node:
+    return Node(
+                package='apriltag_ros',
+                executable='apriltag_node',
+                namespace=f'{drone_id}',
+                output='screen',
+                remappings=[
+                    ('/image_rect', f'/{drone_id}/image_rect'),
+                    ('/camera_info', f'/{drone_id}/camera_info'),
+                    ('/tf',f'/{drone_id}/tf'),
+                    ('/detections',f'/{drone_id}/detections'),
+                ]
+                )
+
+def get_mission_planner_node() -> Node:
+    return Node(
             package='mission_planner',
             executable='mission_planner',
             output='screen',
@@ -60,12 +77,3 @@ def generate_launch_description():
             #     'undetectedTags': ["tag36h11:200"]
             # }],
             )
-
-    # Create the launch description and populate
-    ld = LaunchDescription()
-
-    for i in nodes:
-        ld.add_action(i)
-    ld.add_action(mission_planner)
-
-    return ld
