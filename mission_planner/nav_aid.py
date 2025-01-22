500d5ea4dde32d6bb7c2662c87f8b374b2658fa6
Phua-Lock-Hian <141598807+Phua-Lock-Hian@users.noreply.github.com>
2025-01-22 17:58:18 +0800

    added nav_aid to send packet to turn
    specific packet data pending

---
+++ mission_planner/nav_aid.py
@@ -0,0 +1,41 @@
+
+from cflib.drivers.crazyradio import Crazyradio
+import time
+import sys
+
+
+def turn_command(channel,drone_address=0xff):   
+    cr = Crazyradio(devid=0) #devid = radio dongle id (0 being the first dongle)
+
+    cr.set_channel(channel)
+    cr.set_data_rate(cr.DR_2MPS)
+
+    for i in range(2):
+        # Send multicast packet to P2P port 7
+        cr.set_address((0xff,0xe7,0xe7,0xe7,0xe7)) # sets destination address for outgoing packets
+        cr.set_ack_enable(False) # disable acknowledgement for outgoing packets
+        cr.send_packet( (0xff, 0x80, 0x63, 0x00, drone_address) ) # sends packet to destination address via radio link
+        #change 0x00 above to whichever number yan yew specifies for turning
+        #need 3 numbers for turn left, turn right and u turn
+        print('send turn to ' + str(drone_address))
+       
+
+        time.sleep(0.01)
+    cr.close()
+    return 0
+
+if __name__ == '__main__':
+    try:
+            turn_command(channel=int(sys.argv[1]), drone_address=int(sys.argv[2], 16))
+    except IndexError:
+        print("Please specify channel and drone ID")
+        
+        
+        
+'''
+0xff = broadcast address
+0xe7 = multicast address (vendor specific address)
+0x80 = 
+0x63 = command to control whether crazyflies should keep flying
+0x00 = 
+'''
