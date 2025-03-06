a147d34e572f2d971c537fbec05cd6792bb75de7
Foo Shu Hui <97970820+shuhuiii@users.noreply.github.com>
2025-03-06 18:27:10 +0800

    add turn_left nav adds

---
+++ mission_planner/command_tag_2.py
@@ -0,0 +1,41 @@
+
+from cflib.drivers.crazyradio import Crazyradio
+import time
+import sys
+
+
+def command_tag_2(channel,drone_address=0xff):   
+    cr = Crazyradio(devid=1) #devid = radio dongle id (0 being the first dongle)
+
+    cr.set_channel(channel)
+    cr.set_data_rate(cr.DR_2MPS)
+
+    for i in range(2):
+        # Send multicast packet to P2P port 7
+        cr.set_address((0xff,0xe7,0xe7,0xe7,0xe7)) # sets destination address for outgoing packets
+        cr.set_ack_enable(False) # disable acknowledgement for outgoing packets
+        cr.send_packet( (0xff, 0x80, 0x72, 0x01, drone_address) ) # sends packet to destination address via radio link 
+        print('Tag B ' + str(drone_address))
+       
+
+        time.sleep(0.01)
+    cr.close()
+    return 0
+
+if __name__ == '__main__':
+    try:
+            #print(hex(int(sys.argv[2])))
+            command_tag_2(channel=int(sys.argv[1]), drone_address=(int(sys.argv[2],16)))
+            #land_command(channel=int(sys.argv[1]), drone_address=(int(sys.argv[2])))
+    except IndexError:
+        print("Please specify channel and drone ID")
+        
+        
+        
+'''
+0xff = broadcast address
+0xe7 = multicast address (vendor specific address)
+0x80 = 
+0x63 = command to control whether crazyflies should keep flying
+0x00 = 
+'''
